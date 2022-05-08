94bcfe72ba4a600b8df7ddc48f46859562d3b3da
Vitor <vi.souza.almeida@protonmail.com>
2022-05-08 20:46:18 -0300

    Page component

---
+++ client/components/Page/PageComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Page.scss";
+
+export default function User() {
+  return(
+    <div>
+      <h1 className={styles.heading}>User</h1>
+      <hr/>
+    </div>
+  )
+}
