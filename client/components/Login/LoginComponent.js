d4b69a3b1e189c85d7e8d0bda0f409a951785257
Vitor <vi.souza.almeida@protonmail.com>
2022-05-08 20:45:38 -0300

    Login component

---
+++ client/components/Login/LoginComponent.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Grid, Cell, Textfield, Button, Checkbox } from "react-mdl";
+import Page from "../Page/PageComponent";
+import ReactDOM from 'react-dom'
+
+export default function Login() {
+  ReactDOM.render (
+    <Page heading="Login">
+      SEI LA IRMAO
+      <div style={{ width: "70%", margin: "auto" }}>
+        <Grid>
+          <form style={{ margin: "auto" }}>
+            <Cell col={12}>
+              <Textfield onChange={() => {}} label="Username" />
+            </Cell>
+            <Cell col={12}>
+              <Textfield onChange={() => {}} label="Password" type="password" />
+            </Cell>
+            <Cell col={12}>
+              <Checkbox
+                label="Remember me"
+                ripple
+                style={{ textAlign: "right" }}
+              ></Checkbox>
+            </Cell>
+            <Cell col={12} style={{ textAlign: "right" }}>
+              <a href="#">Forgot password?</a>
+              <Button primary>Login</Button>
+            </Cell>
+          </form>
+        </Grid>
+      </div>
+    </Page>
+  );
+}
