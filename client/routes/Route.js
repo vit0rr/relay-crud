65a4024721a229ea91b2aa778e804b15bbf8dd51
Vitor <vi.souza.almeida@protonmail.com>
2022-05-08 20:47:30 -0300

    Routes

---
+++ client/routes/Route.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { IndexRoute, Route, Redirect } from 'react-router';
+
+import ViewerQuery from './ViewerQuery';
+import AppContainer from '../components/App/AppContainer';
+// import UserContainer from '../components/User/UserContainer';
+import SignupComponent from '../components/Signup/SignupComponent';
+import LoginComponent from '../components/Login/LoginComponent';
+
+export default (
+  <Route path="/" component={AppContainer} queries={ViewerQuery}>
+    {/* <IndexRoute component={UserContainer} queries={ViewerQuery} /> */}
+    <IndexRoute queries={ViewerQuery} />
+    <Route path="signup" component={SignupComponent} />
+    <Route path="login">
+      <LoginComponent></LoginComponent>
+    </Route>
+    <Redirect from="*" to="/" />
+  </Route>
+);
