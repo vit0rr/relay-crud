2382b2aee70eb508721f8570858b6f3e14dbccbc
Vitor <vi.souza.almeida@protonmail.com>
2022-05-17 21:11:15 -0300

    About component

---
+++ client/components/About.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+export default function About() {
+  return (
+    <>
+      <div className="about">
+        <h1>About</h1>
+        <p>
+          This is a project that I made to learn more about GraphQL, Relay, and
+          ReactJS. I was not built this project from zero cause I dont know yet
+          how, and for this reason, Im thinking about writing a long tutorial
+          about GraphQL and Relay in my blog to explain everything that I
+          learning. This project and my future articles that I will write is too
+          to help me for applying to <a href="https://entria.com.br/">Entria</a>
+        </p>
+      </div>
+    </>
+  );
+}
